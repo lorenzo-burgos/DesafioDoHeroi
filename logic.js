9fe2bc62517dea66fe007a77e58832ccd4eec7c9
Lorenzo <72982330+lorenzo-burgos@users.noreply.github.com>
2024-11-21 14:11:07 -0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,12 +1,10 @@
 let herois = [];
 
-// Função para exibir o herói atual
 function mostrarHeroiAtual(nome, nivel) {
     const divAtual = document.getElementById('atual');
     divAtual.innerHTML = `O Herói de nome <strong>${nome}</strong> está no nível de <strong>${nivel}</strong>`;
 }
 
-// Evento para o formulário
 document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -40,14 +38,11 @@ document.querySelector('form').addEventListener('submit', function (event) {
     document.getElementById('xp').value = '';
 });
 
-// Função para classificar os heróis com base no XP
 function classificarHeroi(nome, xp, nivel) {
     herois.push({ nome, xp, nivel });
 
-    // Ordena os heróis com base no XP em ordem decrescente
     herois.sort((a, b) => b.xp - a.xp);
 
-    // Mantém apenas os 5 melhores heróis
     if (herois.length > 5) {
         herois = herois.slice(0, 5);
     }
@@ -55,11 +50,9 @@ function classificarHeroi(nome, xp, nivel) {
     atualizarPodio();
 }
 
-// Função para atualizar o pódio dos heróis mais poderosos
 function atualizarPodio() {
     const podioDiv = document.querySelector('.podio');
-    podioDiv.innerHTML = ''; // Limpa o conteúdo anterior
-
+    podioDiv.innerHTML = ''; 
     herois.forEach((heroi, index) => {
         const heroiDiv = document.createElement('div');
         heroiDiv.innerHTML = `#${index + 1} - <strong>${heroi.nome}</strong> (XP: ${heroi.xp}, Nível: ${heroi.nivel})`;
